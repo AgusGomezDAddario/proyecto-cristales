ca1974d9efd1d57d2db3e32fcf9b97c27c1299af
Enzo Defranco <enzolobod22@gmail.com>
2026-04-08 15:43:42 -0300

    Fix: Ingreso duplicado al anular OT + Fecha/hora faltante en movimientos

--- app/Http/Controllers/OrdenDeTrabajoController.php
+++ app/Http/Controllers/OrdenDeTrabajoController.php
@@ -669,7 +669,7 @@ class OrdenDeTrabajoController extends Controller
 
                     // Crear el movimiento
                     Movimiento::create([
-                        'fecha' => $pago->fecha ?? $orden->fecha,
+                        'fecha' => now(),
                         'monto' => $montoParaGuardar,
                         'concepto_id' => $conceptoId,
                         'medio_de_pago_id' => $pago->medio_de_pago_id,
@@ -795,7 +795,7 @@ class OrdenDeTrabajoController extends Controller
 
                 foreach ($ingresosExistentes as $ingreso) {
                     Movimiento::create([
-                        'fecha' => now()->toDateString(),
+                        'fecha' => now(),
                         'monto' => $ingreso->monto,
                         'concepto_id' => $conceptoAnulacion->id,
                         'medio_de_pago_id' => $ingreso->medio_de_pago_id,
