a60f1ab512ef2508bd51acf0e8b212f936176ce8
Santiago Di Camillo <dicamillosantiago@gmail.com>
2026-04-11 20:18:35 -0300

    add middleware para railway

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,12 +3,17 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 use Inertia\Inertia;
 
 class AppServiceProvider extends ServiceProvider
 {
     public function boot(): void
     {
+        if (app()->environment('production')) {
+            URL::forceScheme('https');
+        }
+
         Inertia::share([
             'flash' => function () {
                 return [
