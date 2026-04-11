3f937346facaf666088e1f25e6afe7ae0700ecca
Santiago Di Camillo <dicamillosantiago@gmail.com>
2026-04-11 19:54:09 -0300

    test vite

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -39,6 +39,6 @@
         <!-- @routes -->
 
         @viteReactRefresh
-        @vite(['resources/js/app.tsx', "resources/js/Pages/{$page['component']}.tsx"])
+        @vite(['resources/js/app.tsx'])
     </body>
 </html>
